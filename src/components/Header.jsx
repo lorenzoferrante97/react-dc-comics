7c2b8aa567e1defcdaaefbd6aa6e1959f5895f27
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-18 16:35:10 +0100

    fix: header comp style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,27 +5,30 @@ export default function Header() {
     return (
 
         <>
-            <header className="flex flex-col md:flex-row justify-between items-center gap-5 p-7 bg-gray-50">
-                {/* logo */}
-                <figure>
-                    <img src="../../public/dc-logo.png" alt="DC Comics logo" />
-                </figure>
+            <header className="bg-gray-50 w-full py-10">
+                {/* header container */}
+                <div className="max-w-[92%] mx-auto flex flex-col md:flex-row justify-between items-center gap-5">
+                    {/* logo */}
+                    <figure>
+                        <img src="../../public/dc-logo.png" alt="DC Comics logo" />
+                    </figure>
 
-                {/* nav */}
-                <nav>
-                    <ul className="flex flex-wrap gap-3 justify-center">
-                        <li>Characters</li>
-                        <li>Comics</li>
-                        <li>Movies</li>
-                        <li>Tv</li>
-                        <li>Games</li>
-                        <li>Collectibles</li>
-                        <li>Videos</li>
-                        <li>Fans</li>
-                        <li>News</li>
-                        <li>Shop</li>
-                    </ul>
-                </nav>
+                    {/* nav */}
+                    <nav className="md:max-lg:max-w-[72%]">
+                        <ul className="flex flex-wrap gap-3 justify-center md:justify-end">
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>Tv</li>
+                            <li>Games</li>
+                            <li>Collectibles</li>
+                            <li>Videos</li>
+                            <li>Fans</li>
+                            <li>News</li>
+                            <li>Shop</li>
+                        </ul>
+                    </nav>
+                </div>
             </header>
         </>
 
