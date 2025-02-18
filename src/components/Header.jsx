4b928fb9c7e491fea4911d471281fde3ac012f6f
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-18 16:22:32 +0100

    add: nav + header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
 
         <>
-            <header>
+            <header className="flex flex-col md:flex-row justify-between items-center gap-5 p-7 bg-gray-50">
                 {/* logo */}
                 <figure>
                     <img src="../../public/dc-logo.png" alt="DC Comics logo" />
@@ -13,7 +13,18 @@ export default function Header() {
 
                 {/* nav */}
                 <nav>
-
+                    <ul className="flex flex-wrap gap-3 justify-center">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Collectibles</li>
+                        <li>Videos</li>
+                        <li>Fans</li>
+                        <li>News</li>
+                        <li>Shop</li>
+                    </ul>
                 </nav>
             </header>
         </>
