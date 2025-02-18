4cc7e1eed98f52c3cba58d2c48e606de4fe36591
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-18 16:52:35 +0100

    add: main content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,11 @@ export default function Main() {
 
     return (
         <>
-            <main>
-                <p>Content goes here</p>
+            <main className="flex justify-center items-center w-full bg-black">
+                {/* main container */}
+                <div className="max-w-[80%] min-h-40 flex justify-center items-center">
+                    <p className="text-white">Content goes here</p>
+                </div>
             </main>
         </>
     );
