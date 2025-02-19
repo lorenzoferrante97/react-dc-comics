316f2d6861a5222676ead8473c8e6d3c3352204c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-19 15:59:43 +0100

    edit: nav links with data files

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+//import data
+import linksMain from "./data/linksMain";
+
 function App() {
 
-  const linksMain = [
-    { id: 1, content: "Characters", url: "#", isActive: false },
-    { id: 2, content: "Comics", url: "#", isActive: true },
-    { id: 3, content: "Movies", url: "#", isActive: false },
-    { id: 4, content: "TV", url: "#", isActive: false },
-    { id: 5, content: "Games", url: "#", isActive: false },
-    { id: 6, content: "Collectibles", url: "#", isActive: false },
-    { id: 7, content: "Videos", url: "#", isActive: false },
-    { id: 8, content: "Fans", url: "#", isActive: false },
-    { id: 9, content: "News", url: "#", isActive: false },
-    { id: 10, content: "Shop", url: "#", isActive: false }
-  ];
+  // const linksMain = [
+  //   { id: 1, content: "Characters", url: "#", isActive: false },
+  //   { id: 2, content: "Comics", url: "#", isActive: true },
+  //   { id: 3, content: "Movies", url: "#", isActive: false },
+  //   { id: 4, content: "TV", url: "#", isActive: false },
+  //   { id: 5, content: "Games", url: "#", isActive: false },
+  //   { id: 6, content: "Collectibles", url: "#", isActive: false },
+  //   { id: 7, content: "Videos", url: "#", isActive: false },
+  //   { id: 8, content: "Fans", url: "#", isActive: false },
+  //   { id: 9, content: "News", url: "#", isActive: false },
+  //   { id: 10, content: "Shop", url: "#", isActive: false }
+  // ];
 
 
   return (
---
+++ src/data/linksMain.js
@@ -0,0 +1,15 @@
+
+const linksMain = [
+    { id: 1, content: "Characters", url: "#", isActive: false },
+    { id: 2, content: "Comics", url: "#", isActive: true },
+    { id: 3, content: "Movies", url: "#", isActive: false },
+    { id: 4, content: "TV", url: "#", isActive: false },
+    { id: 5, content: "Games", url: "#", isActive: false },
+    { id: 6, content: "Collectibles", url: "#", isActive: false },
+    { id: 7, content: "Videos", url: "#", isActive: false },
+    { id: 8, content: "Fans", url: "#", isActive: false },
+    { id: 9, content: "News", url: "#", isActive: false },
+    { id: 10, content: "Shop", url: "#", isActive: false }
+];
+
+export default linksMain;
