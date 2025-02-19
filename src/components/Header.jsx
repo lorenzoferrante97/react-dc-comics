ba3c9d5e48a4009ab81191ce6f1d5aeb49fcf48d
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-19 15:54:52 +0100

    add: nav style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,12 +18,14 @@ export default function Header(props) {
 
                     {/* nav */}
                     <nav className="md:max-lg:max-w-[72%]">
-                        <ul className="flex flex-wrap gap-3 justify-center md:justify-end">
+                        <ul className="flex flex-wrap gap-5 justify-center md:justify-end">
 
                             {
                                 links.map((link) => {
                                     return (
-                                        <li key={link.id} className={` hover:border-b-4 hover:border-blue-400 hover:text-blue-400 py-2 uppercase font-semibold ${link.isActive ? 'border-b-4 border-blue-400 text-blue-400' : ''}`}><a href="{link.url}">{link.content}</a></li>
+                                        <li key={link.id} className={` hover:border-b-4 hover:border-blue-400 hover:text-blue-400 py-2 uppercase font-semibold ${link.isActive ? 'border-b-4 border-blue-400 text-blue-400' : ''}`}>
+                                            <a href="{link.url}">{link.content}</a>
+                                        </li>
                                     );
                                 })
                             }
