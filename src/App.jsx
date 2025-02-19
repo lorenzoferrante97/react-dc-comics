b2e27153d18a100b2011e7bab198163aeeb0dcab
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-19 15:10:59 +0100

    add: global links main array

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,20 @@ import Main from "./components/Main";
 
 function App() {
 
+  const linksMain = [
+    { id: 1, content: "Characters", url: "#", isActive: false },
+    { id: 2, content: "Comics", url: "#", isActive: true },
+    { id: 3, content: "Movies", url: "#", isActive: false },
+    { id: 4, content: "TV", url: "#", isActive: false },
+    { id: 5, content: "Games", url: "#", isActive: false },
+    { id: 6, content: "Collectibles", url: "#", isActive: false },
+    { id: 7, content: "Videos", url: "#", isActive: false },
+    { id: 8, content: "Fans", url: "#", isActive: false },
+    { id: 9, content: "News", url: "#", isActive: false },
+    { id: 10, content: "Shop", url: "#", isActive: false }
+  ];
+
+
   return (
     <>
       <Header />
