361d68c1b296b5fea4f5b49a11ca58e9f875aa3f
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-20 12:00:56 +0100

    add: card structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,18 @@ export default function Card (props) {
 
     return (
 
+        <>
+            {/* card */}
+            <div>
+                {/* card img */}
+                <figure>
+                    <img src="" alt="" />
+                </figure>
+                {/* card title */}
+                <p></p>
+            </div>
+        </>
+
     );
 
 }
