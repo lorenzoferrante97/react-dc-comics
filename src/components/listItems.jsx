f6581f1793850896635eb0870b54d585967428d6
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-20 15:20:08 +0100

    bonus: list item comp structure

---
+++ src/components/listItems.jsx
@@ -0,0 +1,18 @@
+
+
+export default function ListItem (props) {
+
+    return (
+
+        <>
+            {/* icon */}
+            <figure>
+                <img src="" alt="" />
+            </figure>
+            {/* label */}
+            <p></p>
+        </>
+
+    );
+
+}
