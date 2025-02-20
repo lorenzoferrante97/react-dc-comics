4f41deb793afe8b2dac46c3b053512d1a4dde49c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-20 12:55:15 +0100

    add: card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,13 +8,13 @@ export default function Card (props) {
 
         <>
             {/* card */}
-            <div>
+            <div className="flex flex-col gap-4 items-center">
                 {/* card img */}
-                <figure className="w-full">
-                    <img src={card.thumb} alt={card.title} className="w-full" />
+                <figure className="w-full aspect-square overflow-hidden">
+                    <img src={card.thumb} alt={card.title} className="w-full object-cover" />
                 </figure>
                 {/* card title */}
-                <p>{card.title}</p>
+                <p className="font-bold uppercase">{card.title}</p>
             </div>
         </>
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ export default function Main() {
 
     return (
         <>
-            <main className="flex justify-center items-center w-full bg-black text-white">
+            <main className="flex justify-center items-center w-full bg-black text-white p-10">
                 {/* main container */}
                 <div className="max-w-[80%] min-h-40 flex justify-center items-center">
                     <Cards />
